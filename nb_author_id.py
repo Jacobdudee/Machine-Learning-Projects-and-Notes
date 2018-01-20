b123b85e496b2c01e5314f811b5e707ab4f2bae6
Jacob Dodd <jacobdodd94@gmail.com>
2018-01-20 13:55:22 -0800

    Added Naive Bayes notes and file

---
+++ nb_author_id.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+### your code goes here ###
+
+#import modules
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+#create classifier
+authorNB = GaussianNB()
+
+#fitting model
+t0 = time()
+authorNB.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+# getting predictions
+t0 = time()
+pred = authorNB.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+# scoring model
+print accuracy_score(pred, labels_test)
+
+#no. of Chris training emails: 7936
+#no. of Sara training emails: 7884
+#training time: 2.492 s
+#prediction time: 0.232 s
+# score = 0.973265073948
+#########################################################
+
+
